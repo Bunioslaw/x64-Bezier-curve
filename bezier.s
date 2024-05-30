0ad899d5b631f1c4270263741a44455006dd17f0
Adrian Jędrych <adrianlukaszj@gmail.com>
2024-05-30 20:24:53 +0200

    temp

--- bezier.s
+++ bezier.s
@@ -1,3 +1,20 @@
+section .data
+    %define x0      xmm3
+    %define y0      xmm4
+    %define x1      xmm5    ;[RBP+48]
+    %define y1      xmm6    ;[RBP+56]
+    %define x2      xmm7    ;[RBP+64]
+    %define y2      xmm8    ;[RBP+72]
+    %define x3      xmm9    ;[RBP+80]
+    %define y3      xmm10    ;[RBP+88]
+    %define x4      xmm11   ;[RBP+96]
+    %define y4      xmm12   ;[RBP+104]
+    %define width   rdi
+    %define bmp     rcx
+    step dq 0.01
+    one dq 1
+    zero dq 0
+
 section .text
 
 global bezier
@@ -5,18 +22,155 @@ global bezier
 bezier:
     push rbp
     mov rbp, rsp
-    mov rax, rdi
+    push rbx
+    push r12
+    push r13
+    push r14
+    push r15
 
-begin:
-    mov cl, [rax]
-    test cl, cl
-    jz end
-    add cl, 1
-    mov [rax], cl
-    inc rax
-    jmp begin
+    sub rsp, 128
+    vmovdqu [rsp], xmm6
+    vmovdqu [rsp+16], xmm7
+    vmovdqu [rsp+32], xmm8
+    vmovdqu [rsp+48], xmm9
+    vmovdqu [rsp+64], xmm10
+    vmovdqu [rsp+80], xmm11
+    vmovdqu [rsp+96], xmm12
+    vmovdqu [rsp+112], xmm13
+    vmovdqu [rsp+128], xmm14
+    vmovdqu [rsp+144], xmm15
+
+    cvtsi2sd xmm3, r8
+    cvtsi2sd xmm4, r9
+    mov rax, [RBP+48]
+    cvtsi2sd xmm5, rax
+    mov rax, [RBP+56]
+    cvtsi2sd xmm6, rax
+    mov rax, [RBP+64]
+    cvtsi2sd xmm7, rax
+    mov rax, [RBP+72]
+    cvtsi2sd xmm8, rax
+    mov rax, [RBP+80]
+    cvtsi2sd xmm9, rax
+    mov rax, [RBP+88]
+    cvtsi2sd xmm10, rax
+    mov rax, [RBP+96]
+    cvtsi2sd xmm11, rax
+    mov rax, [RBP+104]
+    cvtsi2sd xmm12, rax
+    
+    ; fninit
+    mov rax, one
+    movsd xmm0, qword [rax]     ; xmm0 = 1 - t
+
+    mov rax, zero
+    movsd xmm1, qword [rax]     ; xmm1 = t
+
+    mov rax, step
+    movsd xmm2, qword [rax]     ; xmm2 = step
+
+
+main:
+    ucomisd xmm0, qword [step]
+    jb end
+
+    movsd xmm13, xmm0       ; (1-t)
+    mulsd x3, xmm13
+    mulsd y3, xmm13
+
+    mulsd xmm13, xmm0       ; (1-t)^2
+    mulsd x2, xmm13
+    mulsd y2, xmm13
+
+    mulsd xmm13, xmm0       ; (1-t)^3
+    mulsd x1, xmm13
+    mulsd y1, xmm13
+
+    mulsd xmm13, xmm0       ; (1-t)^4
+    mulsd x0, xmm13
+    mulsd y0, xmm13
+
+    movsd xmm13, xmm1       ; t
+    mulsd x1, xmm13
+    mulsd y1, xmm13
+
+    mulsd xmm13, xmm1       ; t^2
+    mulsd x2, xmm13
+    mulsd y2, xmm13
+
+    mulsd xmm13, xmm1       ; t^3
+    mulsd x3, xmm13
+    mulsd y3, xmm13
+
+    mulsd xmm13, xmm1       ; t^4
+    mulsd x4, xmm13
+    mulsd y4, xmm13
+
+    mov rax, 4
+    cvtsi2sd xmm13, rax
+    mulsd x1, xmm13
+    mulsd y1, xmm13
+    mulsd x3, xmm13
+    mulsd y3, xmm13
+
+    mov rax, 6
+    cvtsi2sd xmm13, rax
+    mulsd x2, xmm13
+    mulsd y2, xmm13
+
+    movsd xmm13, x0
+    addsd xmm13, x1
+    addsd xmm13, x2
+    addsd xmm13, x3
+    addsd xmm13, x4
+
+    movsd xmm14, y0
+    addsd xmm14, y1
+    addsd xmm14, y2
+    addsd xmm14, y3
+    addsd xmm14, y4
+
+    cvttsd2si rdx, xmm13    ; x to draw
+
+    cvttsd2si rbx, xmm14    ; y to draw
+
+    mov r13, rdx
+    ; add r12, rdx
+
+    shl rdx, 2
+    mov rax, width
+    mul rbx
+    shl rbx, 2
+    add rdx, rbx
+    shl rdx, 2
+    add rdx, rcx
+    mov dword[rdx], 0xff00ffff
+
+    subsd xmm0, xmm2
+    addsd xmm1, xmm2
+
+    
+    mov rax, r13
+    jmp main
 
 end:
+    vmovdqu xmm6, [rsp]
+    vmovdqu xmm7, [rsp+16]
+    vmovdqu xmm8, [rsp+32]
+    vmovdqu xmm9, [rsp+48]
+    vmovdqu xmm10, [rsp+64]
+    vmovdqu xmm11, [rsp+80]
+    vmovdqu xmm12, [rsp+96]
+    vmovdqu xmm13, [rsp+112]
+    vmovdqu xmm14, [rsp+128]
+    vmovdqu xmm15, [rsp+144]
+    add rsp, 128
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbx
     mov rsp, rbp
     pop rbp
     ret
--- main.c
+++ main.c
@@ -2,24 +2,37 @@
 #include <allegro5/allegro_primitives.h>
 #include <stdio.h>
 
-extern void bezier(char* bitmap, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5);
+extern int bezier(char* bitmap, int width, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5);
 
 int main(int argc, char **argv) {
+    int width = 800;
+    int height = 600;
+
+    if (argc >= 3){
+        width = atoi(argv[1]);
+        height = atoi(argv[2]);
+    }
 
     // Bitmap and display initialization
     al_init();
     al_init_primitives_addon();
-    ALLEGRO_DISPLAY *display = al_create_display(800, 600);
-    ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
-    ALLEGRO_BITMAP *bitmap = al_create_bitmap(800, 600);
+    ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    ALLEGRO_BITMAP *bitmap = NULL;
+    ALLEGRO_LOCKED_REGION *locked_region = NULL;
+
+    display = al_create_display(width, height);
+    event_queue = al_create_event_queue();
 
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_install_mouse();
     al_register_event_source(event_queue, al_get_mouse_event_source());
 
+    al_set_new_bitmap_format(ALLEGRO_PIXEL_FORMAT_ANY_32_WITH_ALPHA);
+    bitmap = al_create_bitmap(width, height);
 
     al_set_target_bitmap(bitmap);
-    al_clear_to_color(al_map_rgb(255, 255, 255));
+    al_clear_to_color(al_map_rgba(255, 255, 255, 255));
 
     al_set_target_backbuffer(display);
     al_clear_to_color(al_map_rgb(0, 0, 0));
@@ -38,14 +51,22 @@ int main(int argc, char **argv) {
             running = false;
         }
         else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && ev.mouse.button) {
+            printf("Kliknieto na pozycji: (%x, %x)\n", ev.mouse.x, ev.mouse.y); // Wydrukuj pozycję myszy
             p[ind][0] = ev.mouse.x;
             p[ind][1] = ev.mouse.y;
-            al_draw_filled_circle(ev.mouse.x, ev.mouse.y, 5, al_map_rgb(255, 0, 0));
+            al_draw_filled_circle(ev.mouse.x, ev.mouse.y, 5, al_map_rgba(255, 0, 0, 255));
             ind++;
             if (ind > 4){
-                al_clear_to_color(al_map_rgb(255, 255, 255));   // clear bitmap
+                al_clear_to_color(al_map_rgb(255, 255, 255));
+                al_destroy_bitmap(bitmap);
+                bitmap = al_create_bitmap(width, height);
+
+                locked_region = al_lock_bitmap(bitmap, ALLEGRO_PIXEL_FORMAT_ANY_32_WITH_ALPHA, ALLEGRO_LOCK_WRITEONLY);
                 printf_s("Drawing bezier\n");// call bezier function
-                //bezier(bitmap, p[0][0], p[0][1], p[1][0], p[1][1], p[2][0], p[2][1], p[3][0], p[3][1], p[4][0], p[4][1]);
+                printf("%x\n", bezier(locked_region->data, width, p[0][0], p[0][1], p[1][0], p[1][1], p[2][0], p[2][1], p[3][0], p[3][1], p[4][0], p[4][1]));
+                al_unlock_bitmap(bitmap);
+
+                al_draw_bitmap(bitmap, 0, 0, 0);
                 ind = 0;
             }
             al_flip_display();
